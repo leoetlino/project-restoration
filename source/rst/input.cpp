bb205ca0c41bab5fc7481463426aaaa86bc4872a
Léo Lam <leo@leolam.fr>
2019-07-23 17:35:33 +0800

    Add fast equip physical mapping for I/II
    The logical consequence of #16, which maps I/II to ZR+X / ZR+Y.
    
    Fixes #25

--- source/rst/input.cpp
+++ source/rst/input.cpp
@@ -3,36 +3,81 @@
 
 namespace rst {
 
+/// Modified touchscreen state, ignoring item usability checks.
+static game::pad::TouchscreenState s_touchscreen_state;
+
 void UpdatePadState() {
   using namespace game;
 
   auto& controller_mgr = pad::GetControllerMgr();
   auto& state = controller_mgr.state;
-  auto& touchscreen_state = controller_mgr.touchscreen_state;
+  s_touchscreen_state = controller_mgr.touchscreen_state;
 
-  const auto set_touch_btn = [&](pad::Button trigger, pad::TouchscreenButton btn,
-                                 UsableButton btn_to_check) {
-    const bool usable = GetCommonData().usable_btns[u8(btn_to_check)] != ButtonIsUsable::No;
-    if (state.input.buttons.TestAndClear(trigger) && usable)
-      touchscreen_state.buttons.Set(btn);
-    if (state.input.new_buttons.TestAndClear(trigger) && usable)
-      touchscreen_state.new_buttons.Set(btn);
+  const auto set_touch_btn = [&](pad::Button trigger, pad::TouchscreenButton btn) {
+    if (state.input.buttons.TestAndClear(trigger))
+      s_touchscreen_state.buttons.Set(btn);
+    if (state.input.new_buttons.TestAndClear(trigger))
+      s_touchscreen_state.new_buttons.Set(btn);
   };
 
-  set_touch_btn(pad::Button::ZL, pad::TouchscreenButton::PictographBox,
-                UsableButton::PictographBox);
-
+  set_touch_btn(pad::Button::ZL, pad::TouchscreenButton::PictographBox);
   if (state.input.buttons.IsSet(pad::Button::ZR)) {
-    set_touch_btn(pad::Button::A, pad::TouchscreenButton::Ocarina, UsableButton::Ocarina);
-    set_touch_btn(pad::Button::X, pad::TouchscreenButton::I, UsableButton::I);
-    set_touch_btn(pad::Button::Y, pad::TouchscreenButton::II, UsableButton::II);
+    set_touch_btn(pad::Button::A, pad::TouchscreenButton::Ocarina);
+    set_touch_btn(pad::Button::X, pad::TouchscreenButton::I);
+    set_touch_btn(pad::Button::Y, pad::TouchscreenButton::II);
   }
+
+  controller_mgr.touchscreen_state = s_touchscreen_state;
+
+  // Unset unusable buttons.
+  const auto unset_if_not_usable = [&](pad::TouchscreenButton btn, UsableButton btn_to_check) {
+    const bool usable = GetCommonData().usable_btns[u8(btn_to_check)] != ButtonIsUsable::No;
+    if (!usable) {
+      controller_mgr.touchscreen_state.buttons.Clear(btn);
+      controller_mgr.touchscreen_state.new_buttons.Clear(btn);
+    }
+  };
+  unset_if_not_usable(pad::TouchscreenButton::I, UsableButton::I);
+  unset_if_not_usable(pad::TouchscreenButton::II, UsableButton::II);
+  unset_if_not_usable(pad::TouchscreenButton::Ocarina, UsableButton::Ocarina);
+  unset_if_not_usable(pad::TouchscreenButton::PictographBox, UsableButton::PictographBox);
 }
+
+namespace ui::items {
+
+bool IsItemAssignRequested() {
+  using namespace game::pad;
+  const auto& mgr = GetControllerMgr();
+  return mgr.state.input.new_buttons.IsOneSet(Button::X, Button::Y) ||
+         s_touchscreen_state.new_buttons.IsOneSet(TouchscreenButton::I, TouchscreenButton::II);
+}
+
+int GetItemAssignIndex() {
+  using namespace game::pad;
+  const auto& mgr = GetControllerMgr();
+  if (mgr.state.input.new_buttons.IsSet(Button::X))
+    return 1;
+  if (mgr.state.input.new_buttons.IsSet(Button::Y))
+    return 2;
+  if (s_touchscreen_state.new_buttons.IsSet(TouchscreenButton::I))
+    return 0;
+  if (s_touchscreen_state.new_buttons.IsSet(TouchscreenButton::II))
+    return 3;
+  return 1;  // should be unreachable
+}
+
+}  // namespace ui::items
 
 }  // namespace rst
 
 extern "C" {
 RST_HOOK void rst_UpdatePadState() {
   rst::UpdatePadState();
+}
+RST_HOOK bool rst_ui_items_IsItemAssignRequested() {
+  return rst::ui::items::IsItemAssignRequested();
+}
+RST_HOOK int rst_ui_items_GetItemAssignIndex() {
+  return rst::ui::items::GetItemAssignIndex();
 }
 }
--- source/rst/trampolines.s
+++ source/rst/trampolines.s
@@ -1,11 +1,15 @@
 .section ".init"
 .align 4
 
-.macro TRAMPOLINE_R0_RESULT name
+.macro TRAMPOLINE_DECLARE name
 	.global rst_trampoline_\name
 	.type rst_trampoline_\name, %function
 	.align 4
 rst_trampoline_\name:
+.endm
+
+.macro TRAMPOLINE_R0_RESULT name
+	TRAMPOLINE_DECLARE \name
   push {r1-r12, lr}
   vpush {d0-d15}
   bl \name
@@ -20,10 +24,7 @@ rst_dummy:
 TRAMPOLINE_R0_RESULT rst_link_ShouldUseZoraFastSwim
 TRAMPOLINE_R0_RESULT rst_link_SwitchToZoraFastSwim
 
-.global rst_trampoline_rst_MoveScheduledNpcHook
-.type rst_trampoline_rst_MoveScheduledNpcHook, %function
-.align 4
-rst_trampoline_rst_MoveScheduledNpcHook:
+TRAMPOLINE_DECLARE rst_MoveScheduledNpcHook
   mov r4, r1 @ original instruction
   push {r0-r12, lr}
   vpush {s1-s15}
@@ -33,3 +34,19 @@ rst_trampoline_rst_MoveScheduledNpcHook:
   vpop {s16-s31}
   vpop {s1-s15}
   pop {r0-r12, pc}
+
+TRAMPOLINE_DECLARE rst_ui_items_IsItemAssignRequested
+  push {r0-r12, lr}
+  vpush {d0-d15}
+  bl rst_ui_items_IsItemAssignRequested
+  cmp r0, #0
+  vpop {d0-d15}
+  pop {r0-r12, pc}
+
+TRAMPOLINE_DECLARE rst_ui_items_GetItemAssignIndex
+  push {r0-r2, r4-r12, lr}
+  vpush {d0-d15}
+  bl rst_ui_items_GetItemAssignIndex
+  mov r3, r0
+  vpop {d0-d15}
+  pop {r0-r2, r4-r12, pc}
