991a9e4af8a766870f57580310f45f80ac972e09
Léo Lam <leo@leolam.fr>
2019-08-02 22:33:14 +0800

    common: Add BitSet helper

--- source/common/flags.h
+++ source/common/flags.h
@@ -38,4 +38,30 @@ public:
   std::underlying_type_t<FlagType> flags = 0;
 };
 
+// Super simple alternative to std::bitset with a guaranteed storage layout.
+template <size_t N, typename WordType = u32>
+class BitSet {
+public:
+  constexpr void Set(size_t idx) { GetWord(idx) |= 1 << (idx % NumBitsPerWord); }
+  constexpr void Clear(size_t idx) { GetWord(idx) &= ~(1 << (idx % NumBitsPerWord)); }
+  constexpr bool IsSet(size_t idx) const {
+    return (GetWord(idx) & (1 << (idx % NumBitsPerWord))) != 0;
+  }
+
+  constexpr bool TestAndClear(FlagType v) {
+    if (!IsSet(v))
+      return false;
+    Clear(v);
+    return true;
+  }
+
+private:
+  static_assert(std::is_arithmetic_v<WordType>);
+  // Yes, not technically correct, but this will work on any sane machine, including the 3DS...
+  static constexpr size_t NumBitsPerWord = sizeof(WordType) * 8;
+  static constexpr size_t NumWords = (N + NumBitsPerWord - 1) / NumBitsPerWord;
+  constexpr Word& GetWord(size_t idx) { return m_storage[idx / NumBitsPerWord]; }
+  std::array<Word, NumWords> m_storage{};
+};
+
 }  // namespace rst
