7d01ab338e8b4eaa6ad97509b2ff8c3b75fa40f8
Léo Lam <leo@leolam.fr>
2019-08-11 13:58:39 +0800

    collision: Update structs

---
+++ source/game/collision.cpp
@@ -0,0 +1,18 @@
+#include "game/collision.h"
+
+#include <string>
+
+#include "common/debug.h"
+
+namespace game {
+
+void PrintCollision(const Collision* col, size_t count, std::string_view description) {
+  std::string s{description};
+  s += ' ';
+  for (size_t i = 0; i < count; ++i) {
+    s += rst::util::StringFromFormat("%zu=%02x ", i, col[i].flags1);
+  }
+  rst::util::Print(s);
+}
+
+}  // namespace game
--- source/game/collision.h
+++ source/game/collision.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#include <string_view>
+
 #include "common/types.h"
 
 namespace game {
+
+namespace act {
+class Actor;
+}
+
+// FIXME: Incomplete.
+struct CollisionBody {
+  act::Actor* actor;
+};
 
 struct Collision {
   u8 gap_0[14];
@@ -13,9 +24,16 @@ struct Collision {
   u8 flags0;
   u8 flags1;
   u8 field_17;
-  u8 gap_18[12];
+  u8 gap_18[4];
+  CollisionBody* body;
+  u8 gap_20[4];
   u32 field_24;
-  u8 gap_28[40];
+  u8 gap_28[0x20];
+  float field_48;
+  u8 gap_4C[4];
 };
+static_assert(sizeof(Collision) == 0x50);
+
+void PrintCollision(const Collision* col, size_t count, std::string_view description = "");
 
 }  // namespace game
