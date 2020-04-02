e972eb415977fa76dc22bcbf32fab4787ac70669
Léo Lam <leo@leolam.fr>
2020-04-02 17:57:42 +0200

    Replace Tatl's message about Odolwa's eye
    Since Odolwa's new eyeball was removed, let's replace Tatl's message
    about it with a warning about his dances.
    
    Fixes #65

--- source/game/message.cpp
+++ source/game/message.cpp
@@ -61,6 +61,14 @@ bool MessageData::Get(u16 id, Message* msg) const {
 }
 
 extern "C" RST_HOOK bool rst_MessageDataGet(MessageData* self, u16 id, Message* out) {
+  // Extension: replace Tatl's message about Odolwa's eye with a warning about Odolwa's dances.
+  if (id == 0x195e) {
+    if (!self->Get(0x1962, out))
+      return false;
+    out->msgid = 0x195e;
+    return true;
+  }
+
   return self->Get(id, out);
 }
 
